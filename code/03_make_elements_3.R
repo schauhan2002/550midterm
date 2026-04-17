b10c29800b1d1bece933b0fb37ed1eedf54cc7c4
Sakshi Chauhan <sakshi.chauhan@emory.edu>
2026-04-16 22:37:27 -0400

    Add Element 3 code to new repo structure

--- code/03_make_elements_3.R
+++ code/03_make_elements_3.R
@@ -1,5 +1,41 @@
-## Midterm Report Element #3 (Sakshi)
+# Midterm Report Element #3 (Sakshi)
 
 cleandata <- readRDS(
   file = here::here("data/cleandata.rds")
+)
+
+library(gtsummary)
+library(ggplot2)
+
+# Table of outcome frequencies and percentages by treatment group 
+outcome_table <- cleandata %>%
+  select(arm, withdraw2) %>%
+  tbl_cross(
+    row = arm,
+    col = withdraw2,
+    percent = "row"
+  ) %>%
+  bold_labels()
+saveRDS(
+  outcome_table,
+  file = here::here("output/outcome_table.rds")
+)
+
+# Bar plot of withdrawal reasons by treatment arm
+withdraw_plot <- cleandata %>%
+  filter(!is.na(withdraw2)) %>%
+  ggplot(aes(x = withdraw2, fill = arm)) +
+  geom_bar(position = "dodge") +
+  labs(
+    x = "Reason for withdrawal",
+    y = "Number of patients",
+    fill = "Treatment arm",
+    title = "Reasons for withdrawal/discontinuation of therapy by treatment arm"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+saveRDS(
+  withdraw_plot,
+  file = here::here("output/withdraw_plot.rds")
 )
